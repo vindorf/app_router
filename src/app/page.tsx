8af402d1631a4ab14bc5e7c41ce4e81667bea0c2
321go <gehgo2486@yahoo.com>
2024-03-23 17:32:27 +0100

    hulk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,10 +27,29 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col justify-center items-center ml-16">
-      <h1 className="text-xl mt-24 mb-8">Main Page</h1>
+    <main className="flex flex-col justify-start mx-auto ml-44 items-center relative"
+    style={{ 
+      width: '80vw', 
+      height: '100vh', 
+      overflow: 'hidden' 
+    }}
+    >
+      <div
+       className="text-white absolute inset-0 flex flex-col justify-start items-center m-auto" 
+       style={{ 
+         backgroundImage: `url('https://m.media-amazon.com/images/M/MV5BMzQwZDg1MGEtN2E5My00ZDJlLWI4MzItM2U2MjJhYzlkNmEzXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_SX300.jpg')`,
+         backgroundRepeat: 'no-repeat',
+         backgroundSize: 'cover',
+         width: '70%',
+         height: '100%'
+       }}
+      >
+        <div className="mt-40 mb-8 w-full bg-zinc-900 opacity-70">
+        <b className="text-[50px]">Main Page</b>
+        </div>
+      
       {session && (
-        <div className="flex flex-col items-center gap-3">
+        <div className="w-full flex flex-col items-start p-4  rounded mt-14 gap-3 bg-zinc-900 opacity-80">
           <b>Welcome {toCap(session?.user?.role)}</b>
           <div className="grid grid-cols-2 font-extralight text-right gap-2">
             <p>Name:</p>
@@ -39,16 +58,14 @@ export default function Home() {
             <p>{session?.user?.email} </p>
           </div>
           <DeleteBtn
-            className='w-30 font-extralight text-xs'
+            className='w-30 font-extralight text-xs text-zinc-700 rounded'
             label="Delete account"
             value={`${session?.user?.email}`}
             onDelete={handleConfirm}
           />
         </div>
       )}
-      <div>
-        <img src="/images/seehorse.png" className="w-[600px]" />
-      </div>
+      </div>     
     </main>
   );
 }
