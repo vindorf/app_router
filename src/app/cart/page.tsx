c70b95cc3cc6c5c8e6a3fc9eac1099f0d60833e0
321go <gehgo2486@yahoo.com>
2024-04-13 15:58:21 +0200

    add_nothing

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,40 +1,34 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useCartStore } from "../store/cartStore";
 import { useSession } from "next-auth/react";
-
+//import axios from "axios";
 
 const CartPage = () => {
-  const {data:session} = useSession();
+  const { data: session } = useSession();
   const uMail = session?.user?.email;
   const [msg, setMsg] = useState("");
-    const {cart, removeFromCart, addToCart, clearCart, fetch} = useCartStore((state) => ({
+  const { fetch, cart, removeFromCart } = useCartStore((state) => ({
     fetch: state.fetch,
     cart: state.cart,
     removeFromCart: state.removeFromCart,
-    addToCart: state.addToCart,
-    clearCart: state.clearCart
-  }))
+  }));
 
-  const handleDelete = async (ID:string) => {
+  const handleDelete = async (ID: string) => {
     await removeFromCart(uMail, ID);
     setMsg("Removed from cart");
-    await fetch(uMail)
+    await fetch(uMail);
   };
 
-
-
   useEffect(() => {
-  if(session) {
-    fetch(uMail);
-  }
-  
-},[]);
+    if (session) {
+      fetch(uMail);
+    }
+  }, []);
 
-
-// console.log('MAIL', uMail)
-// console.log('CART', cart)
+  // console.log('MAIL', uMail)
+  //console.log('CART', defCart)
 
   return (
     <div className="mt-5 ml-64 mr-16">
@@ -42,22 +36,21 @@ const CartPage = () => {
         <div className="mt-40 mb-8 w-full bg-zinc-900 opacity-80 px-4 rounded flex flex-col">
           <b className="text-[50px]">Your Cart User </b>
           <div className="flex items-center justify-end mb-2">
-          <Link className="bg-zinc-900 hover:underline" href="/products">
-            Back
-          </Link>        
-          </div>       
+            <Link className="bg-zinc-900 hover:underline" href="/products">
+              Back
+            </Link>
+          </div>
         </div>
-        <div className="h-[30px] text-zinc-500">
-        {msg && <p>{msg} </p>}
-        </div>
+        <div className="h-[30px] text-zinc-500">{msg && <p>{msg} </p>}</div>
       </div>
       <p>Cart Items {cart && cart.length} </p>
-      {cart && cart.map((e:any, i:any) => 
-      <div key={i} className="border p-2">
-      <p>{e._id} </p>
-      <button onClick={() => handleDelete(e._id)}>Delete</button>
-      </div>
-      )}
+      {cart &&
+        cart.map((e: any, i: any) => (
+          <div key={i} className="border p-2">
+            <p>{e._id} </p>
+            <button onClick={() => handleDelete(e._id)}>Delete</button>
+          </div>
+        ))}
     </div>
   );
 };
--- src/app/store/cartStore.js
+++ src/app/store/cartStore.js
@@ -1,24 +1,17 @@
-
 import axios from "axios";
 import { create } from "zustand";
-
-
-
 
 export const useCartStore = create((set) => ({
   cart: [],
   fetch: async (email) => {
-    const resp = await axios.get(`/api/cart?email=${email}`)
-    set({cart: await resp.data.cart})
+    const resp = await axios.get(`/api/cart?email=${email}`);
+    set({ cart: await resp.data.cart });
   },
-  //addToCart: (product) => set((state) => ({cart: [...state.cart, product]}) ) ,
   addToCart: async (email, _id) => {
-    await axios.post('/api/cart/',{email: email, _id:_id});
+    await axios.post("/api/cart/", { email: email, _id: _id });
   },
-  // removeFromCart: (productID) => set((state) => ({cart: state.cart.filter(product => product.id !== productID)})) ,
   removeFromCart: async (email, _id) => {
-    await axios.put('/api/cart/', {email: email, _id: _id});
+    await axios.put("/api/cart/", { email: email, _id: _id });
   },
-  clearCart: () => set({cart: []}),
+  clearCart: () => set({ cart: [] }),
 }));
-
